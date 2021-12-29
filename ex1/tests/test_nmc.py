7d9eedfe576c8dc5639e07a79abf4adb01854c56
rmura498 <76777353+rmura498@users.noreply.github.com>
2021-12-29 17:59:26 +0100

    added basic skeleton for testing

---
+++ ex1/tests/test_nmc.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+from nmc import NMC
+
+
+class TestNMC(unittest.TestCase):
+
+    def SetUp(self):
+        n_samples=100
+        n_features=20
+        self.x=np.zeros(shape=(n_samples, n_features))
+        self.y=np.ones(shape=(n_samples,))
+
+    def test_fit(self):
+        pass
+
+    def test_predict(self):
+        pass
